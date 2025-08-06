836a3bb7f68bc1016ca7acff9e7b9fa49c957bb0
Sharrrkkk <projectwebmaster3@gmail.com>
2025-08-06 16:36:19 -0400

    Update some information in the pyproject.toml file, update some information in the __about__.py file, leave cli.py ready to run without main if called directly, but it continues to work if called from main.

--- src/word_game_solver/__about__.py
+++ src/word_game_solver/__about__.py
@@ -4,7 +4,7 @@ __all__ = ["__title__", "__version__", "__summary__", "__description__", "__url_
 
 
 __title__ = "word_game_solver"
-__version__ = "1.0"
+__version__ = "1.0.0"
 __summary__ = "A solver for word-based games."
 __description__ = ("Currently, only the **Word Finder** solver is implemented."
                    " It searches for all possible words of a specific length" 
@@ -13,7 +13,7 @@ __description__ = ("Currently, only the **Word Finder** solver is implemented."
 __url__ = "https://github.com/Sharrrkkk/word_game_solver"
 __docs_url__ = "https://github.com/Sharrrkkk/word_game_solver#README.md"
 __issues_url__ = "https://github.com/Sharrrkkk/word_game_solver/issues"
-__status__ = "Beta"
+__status__ = "Production/Stable"
 __keywords__ = "anagram, anagrams, game, generate, word, words, solver, finder"
 __author__ = "Sharrrkkk"
 __maintainer__ = "Sharrrkkk"
